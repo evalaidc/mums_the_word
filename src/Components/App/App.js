8982f9f402cd5a4b44d87a7a00474fb0a755f29d
Eva Lai <evalai.dc@gmail.com>
2017-04-16 19:04:57 -0400

    changed links for axios

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -43,7 +43,7 @@ class App extends Component {
               render={() => {
                 return (
                   <SpotsContainer
-                  url='http://localhost:3101/api/spots'
+                  url='/api/spots'
                   interval={2000}
                   />
                 )
