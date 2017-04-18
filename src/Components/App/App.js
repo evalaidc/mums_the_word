b35817dd1c837b7b8c02c0ded744146281a94419
Eva Lai <evalai.dc@gmail.com>
2017-04-18 10:41:14 -0400

    attempt at changing routes to be able to redirect

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -23,14 +23,14 @@ class App extends Component {
           <nav>
             <div className="links">
             <Link to="/mums_the_word/"> About </Link>
-            <Link to="/mums_the_word/Home"> List Spots </Link>
+            <Link to="/mums_the_word/Home/"> List Spots </Link>
             </div>
-            <Link to='/mums_the_word/Home'>
+            <Link to='/mums_the_word/Home/'>
               <div id="iconimage"></div>
             </Link>
             <div className="links">
-            <Link to="/mums_the_word/Home/NewForm"> Make a New Mark </Link>
-            <Link to="/mums_the_word/Home/Map"> Map </Link>
+            <Link to="/mums_the_word/Home/NewForm/"> Make a New Mark </Link>
+            <Link to="/mums_the_word/Home/Map/"> Map </Link>
             </div>
           </nav>
           <main>
